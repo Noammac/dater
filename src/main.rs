872386308120d1b9e5cc634a3d54672fc916a0b6
Noam Barnea <noammac.yuval2@gmail.com>
2021-08-01 16:37:41 +0300

    Added formatting to all messages

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,10 @@ fn main() {
     let ord = now.ordinal();
     let year = now.year();
 
+    let underline = style::Underline;
+    let bold = style::Bold;
+    let reset = style::Reset;
+
     let msg: String = match env::var("COUNTDOWN_DATE") {
         Ok(val) => match NaiveDate::parse_from_str(&val, "%F") {
             Ok(dt) => {
@@ -18,30 +22,49 @@ fn main() {
                     " {red}{bold}{delta}{reset} days left",
                     delta = (Local.from_utc_date(&dt) - now).num_days(),
                     red = color::Fg(color::Red),
-                    bold = style::Bold,
-                    reset = style::Reset
+                    bold = bold,
+                    reset = reset
                 )
             }
             Err(_) => {
-                format!(" ERROR: misformatted countdown date {}", val)
+                format!(
+                    " {bold}ERROR:{reset} misformatted countdown date {date}",
+                    bold = bold,
+                    reset = reset,
+                    date = val
+                )
             }
         },
         Err(_) => String::new(),
     };
 
     println!(
-        "Today is the {} day of the year {}, or {:#X} in hex.{}",
-        ordinate(ord),
-        year,
-        ord,
-        msg
+        "Today is the {underline}{day}{reset} day of the year {bold}{year}{reset}, or {underline}{ord:#X}{reset} in hex.{msg}",
+        underline = underline,
+        bold = bold,
+        reset = reset,
+        day = ordinate(ord),
+        year = year,
+        ord = ord,
+        msg = msg
     );
 
     if ord == 256 {
-        println!("Happy day of the Programmer!");
+        println!(
+            "Happy {bold}Day of the Programmer{reset}!",
+            bold = bold,
+            reset = reset
+        );
     }
     if (now.month() == 11) & (now.day() == 20) {
-        println!("Happy birthday! You are now {} years old!", year - 2002);
+        println!(
+            "Happy birthday! You are now {invert}{bold}{purple}{age}{reset} years old!",
+            invert = style::Invert,
+            bold = bold,
+            purple = color::Fg(color::Magenta),
+            reset = reset,
+            age = year - 2002
+        );
     }
 }
 
